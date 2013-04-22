aba9b90cecbb941374e0cf2155dbea5e8af87356
Dan Wolff <dan.d.wolff@gmail.com>
2013-04-22 12:01:49 +0300

    Add license information to the source code; LICENSE.txt -> LICENSE-GPL.txt

--- hi_n_lo.py
+++ hi_n_lo.py
@@ -1,8 +1,29 @@
 #! /usr/bin/env python3
 
-# example usage (the two variants will do the exact same thing):
+
+# Hi-n-lo - filter out the highs and the lows
+# Copyright (C) 2013  Dan Wolff
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Example usage
+# -------------
+# The two variants will do the exact same thing:
 # ./hi_n_lo.py -s 0.03 < data/marketdata.csv > data/marketdata-filtered.csv
 # ./hi_n_lo.py -s 0.03 -i data/marketdata.csv -o data/marketdata-filtered.csv
+
 
 import sys
 import csv
