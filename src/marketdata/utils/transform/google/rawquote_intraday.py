a3d864131fccfe49f7045de16c4dc55845f8476e
Nirdharsh Shetty <knshetty@live.com>
2013-05-29 20:29:48 +0300

    Refactor: Restructure "__preprocess" method, rename variables, & modify comments.

--- src/marketdata/utils/transform/google/rawquote_intraday.py
+++ src/marketdata/utils/transform/google/rawquote_intraday.py
@@ -5,105 +5,104 @@ import re
 
 
 class TranformIntradayQuote(object):
-    '''Purge, normalize, & convert
+    '''Purge, normalize, & convert data within raw quote.
     '''
 
-    def __init__(self, marketdata, interval):
+    def __init__(self, rawquote, interval):
         '''Constructor
         '''
-        self.marketdata = marketdata
+        self.rawquote = rawquote
         self.interval = interval
         self.quote = {}
         self.__preprocess()
 
     def __preprocess(self):
-        #####
-        #
-        # Purge> Harvest header(DCHLOV) & remove metadata
-        #
-        #####
-        def __harvestHeader():
-            for i in self.marketdata:
-                # search for 'COLUMNS=' only at the start
+        # purge action: harvest header(DCHLOV) & remove the metadata
+        quote_header = [self.__harvestHeader()]
+        self.__removeMetadata()
+
+        # normalization action: isolate DCHLOV into two parts: (i)D & (ii)CHLOV
+        i = self.__isolateDateFromCHLOV()
+        running_ts = i[0]
+        chlov = i[1]
+
+        # conversion action: D to (i)unix(uts) & (ii)datetime(dts)
+        timestamps = self.__uts_dts(running_ts)
+        unix_ts = timestamps[0]
+        datetime_ts = timestamps[1]
+
+        # normalization action: merge datasets (i)D & (ii)CHLOV
+        intradayQuote_dts = self.__mergeNpArrays(
+                                self.__convertToNpArray(datetime_ts), chlov)
+        intradayQuote_uts = self.__mergeNpArrays(
+                                self.__convertToNpArray(unix_ts), chlov)
+
+        # produce quote with (i)header (ii)DCHLOV(dts) & (iii)DCHLOV(uts)
+        self.quote['header'] = quote_header
+        self.quote['dts_quote'] = intradayQuote_dts
+        self.quote['uts_quote'] = intradayQuote_uts
+
+    def __harvestHeader(self):
+            for i in self.rawquote:
+                # search for 'COLUMNS=' only at the start of a string
                 if re.search('\ACOLUMNS=', i):
                     return i.split('=')[1].strip('\n').lower()
 
-        def __removeMetadata():
-            for i in self.marketdata:
-                # matches for 'a' only at the start
-                if re.match('\Aa', i):
-                    del self.marketdata[0: self.marketdata.index(i)]
-                    break
+    def __removeMetadata(self):
+        for i in self.rawquote:
+            # matches for 'a' only at the start of a string
+            if re.match('\Aa', i):
+                del self.rawquote[0: self.rawquote.index(i)]
+                break
 
-        marketdata_header = [__harvestHeader()]
-        __removeMetadata()
+    def __isolateDateFromCHLOV(self):
+        # initialize numpy array for CHLOV dataset
+        chlov = np.zeros((len(self.rawquote), 5))
 
-        #####
-        #
-        # Normalize> Split marketdata into: (i)CHLOV & (ii)Date
-        #
-        #####
-        # initialize numpy array size to house CHLOV dataset
-        chlov = np.zeros((len(self.marketdata), 5))
-
-        # initialize array to house Date(raw running-timestamps)
+        # initialize array for D(running-timestamps) dataset
         running_ts = []
 
         # isolate D from CHLOV
         index = 0
-        for line in self.marketdata:
-            line = line.strip('a')  # normalize initial timestamp
+        for line in self.rawquote:
+            line = line.strip('a')  # normalize the very first timestamp
             listFromLine = line.split(',')
             chlov[index, :] = listFromLine[1:6]  # extract CHLOV
-            running_ts.append(int(listFromLine[0]))  # extract running-timestamps
+            running_ts.append(int(listFromLine[0]))  # extract D
             index += 1
+        return [running_ts, chlov]
 
-        #####
-        #
-        # Convert> Date(running-timestamps) to: (i)unix-timestamps (ii)datetime-timestamps
-        #
-        #####
-        # initialize arrays to house datetime-timestamp & unix-timestamp
-        datetime_ts = []
-        unix_ts = []
+    def __uts_dts(self, running_ts):
+        # initialize arrays for datetime-timestamp & unix-timestamp datasets
+        dts = []
+        uts = []
 
-        def __populateUnix_ts(ts):
-            unix_ts.append(ts)
+        def __populateUTS(ts):
+            uts.append(ts)
 
-        def __populateDatetime_ts(ts):
-            datetime_ts.append(datetime.fromtimestamp(ts))
+        def __populateDTS(ts):
+            dts.append(datetime.fromtimestamp(ts))
 
         def __buildTimeDatasets(ts):
-            __populateUnix_ts(ts)
-            __populateDatetime_ts(ts)
+            __populateUTS(ts)
+            __populateDTS(ts)
 
-        # convert running-timestamps to unix-timestamps
+        # convert running-timestamp to unix-timestamp
         for x in running_ts:
-            if x > 500:  # Get the initial timestamp
+            if x > 500:  # get the initial timestamp
                     z = x
                     __buildTimeDatasets(x)
             else:
-                    y = z + x * int(self.interval)  # multiply the running-timestamps with interval
+                    y = z + x * int(self.interval)
                     __buildTimeDatasets(y)
+        return [uts, dts]
 
-        #####
-        #
-        # Normalize> Merge datasets: (i)CHLOV & (ii)Date
-        #
-        #####
-        def __convertToNpArray(arr):
-            na = np.array(arr)
-            return na.reshape((len(na), 1))
+    def __convertToNpArray(self, arr):
+        na = np.array(arr)
+        return na.reshape((len(na), 1))
 
-        def __mergeNpArrays(na1, na2):
-            return np.concatenate((na1, na2), axis=1)
-
-        intradayQuote_dts = __mergeNpArrays(__convertToNpArray(datetime_ts), chlov)
-        intradayQuote_uts = __mergeNpArrays(__convertToNpArray(unix_ts), chlov)
-
-        self.quote['header'] = marketdata_header
-        self.quote['dts_quote'] = intradayQuote_dts
-        self.quote['uts_quote'] = intradayQuote_uts
+    def __mergeNpArrays(self, a, b):
+        return np.concatenate((a, b), axis=1)
 
     def __getDCHLOV(self, dateseries):
         q = {}
