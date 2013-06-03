83fcf1607734b88e19452f1e00e27a33425c78c7
Nirdharsh Shetty <knshetty@live.com>
2013-06-04 00:43:04 +0300

    Feature: Add DPO-CHUNK sequencing functionality

---
+++ src/sequences/io/trend/price/dpo/s_chunk.py
@@ -0,0 +1,21 @@
+'''
+Sequential Price-Trend Models: DPO with Price-Bar-Chunk (DPC)
+'''
+import numpy
+import json
+
+from techmodels.overlays.trend.price.chunk import sign_chunker
+from techmodels.indicators.trend.price.dpo import DPOIndicator
+
+
+def dpo_chunk(data, n=10, getter=lambda x: x):
+    prices = numpy.array(map(getter, data))
+    dpo = DPOIndicator(prices, n)
+    chunks = sign_chunker(dpo.indicator(), data)
+    return chunks
+
+
+def macd_chunk_json(data, pricetype=u'close'):
+    python_list = json.loads(data)
+    chunks = dpo_chunk(python_list, getter=lambda x: x[pricetype])
+    return json.dumps(chunks)
