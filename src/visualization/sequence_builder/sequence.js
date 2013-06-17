dac8a65479c36e859aad9e10c7a3e12b1812b9ad
Dan Wolff <dan.d.wolff@gmail.com>
2013-06-17 07:50:41 +0300

    Add sequence: the underlying sequence structure library

---
+++ src/visualization/sequence_builder/sequence.js
@@ -0,0 +1,194 @@
+var Sequence = (function ($) {
+
+	function Sequence(options) {
+		// blocks[block_id] = block
+		this.blocks = {};
+
+		// edges[edge_id] = edge
+		this.edges = {};
+
+		if (options) {
+			this.set(options);
+		}
+	}
+
+
+
+	Sequence.prototype = {
+		set: function (sequence) {
+			var i;
+
+			if (sequence.blocks) {
+				for (i = 0; i < sequence.blocks.length; i++) {
+					this.set_block(sequence.blocks[i])
+				}
+			}
+
+			if (sequence.edges) {
+				for (i = 0; i < sequence.edges.length; i++) {
+					this.set_edge(sequence.edges[i])
+				}
+			}
+		},
+
+		set_block: function (block) {
+			this.blocks[block.id] = block;
+		},
+
+		get_block: function (block_id) {
+			return this.blocks[block_id];
+		},
+
+		delete_block: function (block_id) {
+			delete this.blocks[block_id];
+		},
+
+		set_edge: function (edge) {
+			this.edges[edge.id] = edge;
+		},
+
+		get_edge: function (edge_id) {
+			return this.edges[edge_id];
+		},
+
+		delete_edge: function (edge_id) {
+			delete this.edges[edge_id];
+		},
+
+		// O(n)
+		get_edges: function (block_id) {
+			var i, edge;
+			var inputs = [];
+			var outputs = [];
+
+			// Loop over the edges, add any that match the `block_id` to the result
+			for (i in this.edges) {
+				edge = this.edges[i];
+
+				if (edge.from === block_id) {
+					outputs.push(edge);
+				} else if (edge.to === block_id) {
+					inputs.push(edge);
+				}
+			}
+
+			return {
+				inputs: inputs,
+				outputs: outputs,
+			};
+		},
+
+		// O(n)
+		get_sources: function () {
+			// Each edge without a `from` represents a source
+			var i;
+			var sources = [];
+
+			// Keep a map of blocks that do have inputs, i.e. they are not sources
+			var not_sources = {};
+
+			for (i in this.edges) {
+				not_sources[this.edges[i].to] = true;
+			}
+
+			for (i in this.blocks) {
+				if (!not_sources[i]) {
+					sources.push(this.blocks[i]);
+				}
+			}
+
+			return sources;
+		},
+
+		sort_blocks: function () {
+			var i;
+			var self = this;
+			var sources = self.get_sources();
+			var graph = {
+				num_branches: 1,
+
+				// matrix[branch][time] = block
+				matrix: [[]],
+			};
+
+			// block_info[block_id] = {inputs_left: number_of_inputs, suggested_next: {branch:x, time:x}}
+			var block_info = get_block_info(self);
+
+			for (i = 0; i < sources.length; i++) {
+				add_block(sources[i].id, 0, 0);
+			}
+
+			function get_block_info(self) {
+				var i;
+				var inputs = {};
+
+				for (i in self.blocks) {
+					inputs[self.blocks[i].id] = { inputs_left: 0, suggested_next: null };
+				}
+
+				for (i in self.edges) {
+					inputs[self.edges[i].to].inputs_left++;
+				}
+
+				return inputs;
+			}
+
+			function add_block(block_id, branch, time) {
+				var info = block_info[block_id];
+				info.inputs_left--;
+
+				if (!info.suggested_next || info.suggested_next.time < time) {
+					info.suggested_next = { branch: branch, time: time };
+				}
+
+				// Are there any more inputs to wait for?
+				if (0 < info.inputs_left) {
+					return;
+				}
+
+				// add this block to the graph now
+				branch = info.suggested_next.branch;
+				time = info.suggested_next.time;
+
+				// Switch branch if the current position is occupied
+				if (graph.matrix[branch][time]) {
+					branch = graph.num_branches++;
+					graph.matrix[branch] = [];
+					// console.log( 'create new branch "%d" for %s', branch, block_id );
+				}
+
+				graph.matrix[branch][time] = block_id;
+				
+				var i;
+				var outputs = self.get_edges(block_id).outputs;
+
+				for (i = 0; i < outputs.length; i++) {
+					add_block(outputs[i].to, branch, time + 1);
+				}
+			}
+
+			function transform_graph(graph) {
+				var branch, time;
+				var result = {};
+
+				for (branch in graph.matrix) {
+					for (time in graph.matrix[branch]) {
+						result[graph.matrix[branch][time]] = {
+							branch: +branch,
+							time: +time
+						};
+					}
+				}
+
+				return result;
+			}
+
+			return transform_graph(graph);
+		}
+	};
+
+	function sort_blocks(self) {
+	}
+
+	return Sequence;
+}(jQuery));
---
+++ src/visualization/sequence_builder/tests/test_sequence.js
@@ -0,0 +1,92 @@
+(function () {
+
+module('Sequence');
+
+var data = {
+	// This represents the following sequence.
+	// a b
+	// |\|
+	// c d
+	// | |\
+	// | e f
+	// |/
+	// g
+	blocks: [
+		{ id: 'a' },
+		{ id: 'b' },
+		{ id: 'c' },
+		{ id: 'd' },
+		{ id: 'e' },
+		{ id: 'f' },
+		{ id: 'g' }
+	],
+	edges: [
+		{ id: 1, from: 'a', to: 'c' },
+		{ id: 2, from: 'a', to: 'd' },
+		{ id: 3, from: 'b', to: 'd' },
+		{ id: 4, from: 'd', to: 'e' },
+		{ id: 5, from: 'd', to: 'f' },
+		{ id: 6, from: 'c', to: 'g' },
+		{ id: 7, from: 'e', to: 'g' }
+	]
+};
+
+test('sequence.get_edges', 3, function () {
+	var seq = new Sequence(data);
+
+	deepEqual(seq.get_edges('a'), {
+		inputs:[],
+		outputs:[
+			seq.get_edge(1),
+			seq.get_edge(2)
+		]
+	}, 'No inputs, outputs');
+
+	deepEqual(seq.get_edges('c'), {
+		inputs:[seq.get_edge(1)],
+		outputs:[seq.get_edge(6)]
+	}, 'Inputs and outputs');
+
+	deepEqual(seq.get_edges('g'), {
+		inputs:[seq.get_edge(6), seq.get_edge(7)],
+		outputs:[]
+	}, 'Inputs, no outputs');
+
+});
+
+
+test('sequence.get_sources', 1, function () {
+	var seq = new Sequence(data);
+
+	deepEqual(seq.get_sources(), [
+		seq.get_block('a'),
+		seq.get_block('b')
+	], 'Get 2 sources');
+});
+
+test('sequence.sort_blocks', 1, function () {
+	var seq = new Sequence(data);
+
+	// time\branch  0 1 2 3
+	//
+	// 0.           a b
+	//              |\ \
+	// 1.           c `-d
+	//               \  |\
+	// 2.             \ e f
+	//                 \|
+	// 3.               g
+
+	deepEqual(seq.sort_blocks(), {
+		a: { time: 0, branch: 0 },
+		b: { time: 0, branch: 1 },
+		c: { time: 1, branch: 0 },
+		d: { time: 1, branch: 2 },
+		e: { time: 2, branch: 2 },
+		f: { time: 2, branch: 3 },
+		g: { time: 3, branch: 2 }
+	}, 'Sort blocks');
+
+});
+
+})();
